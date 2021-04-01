0fc84f5047ddfb6668f0b22a1ad358b7932e9bf6
imelon2 <imelon279@naver.com>
2021-04-01 14:29:19 +0900

    4.1

---
+++ src/p04/textbook/exercise/Exercise01.java
@@ -0,0 +1,11 @@
+package p04.textbook.exercise;
+
+public class Exercise01 {
+	public static void main(String[] args) {
+		// 1번문제
+		// 조건문: if, switch
+		// 반복문: for, while, do while
+		
+		
+	}
+}
---
+++ src/p04/textbook/exercise/Exercise05.java
@@ -0,0 +1,8 @@
+package p04.textbook.exercise;
+
+public class Exercise05 {
+	public static void main(String[] args) {
+		
+		
+	}
+}
